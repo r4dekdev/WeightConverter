405ef496ca18da094a8eb7ae6889f9a313c6ea8e
Radosław Domagała <contact.dompollab@gmail.com>
2024-05-31 10:26:19 +0200

    Added files

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            double kgVal = 0.45359237;
+            double poundVal = 2.20462262;
+
+            Console.WriteLine("1. Convert pounds to kilograms.");
+            Console.WriteLine("2. Convert kilograms to pounds.");
+            Console.Write("Your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter value in pounds: ");
+                    int pounds = int.Parse(Console.ReadLine());
+                    var toKg = pounds * kgVal;
+                    Console.WriteLine($"{pounds} lbs = {Math.Round(toKg)} kg");
+                    break;
+                
+                case 2:
+                    Console.Write("Enter value in kilograms: ");
+                    int kilograms = int.Parse(Console.ReadLine());
+                    var toLb = kilograms * poundVal;
+                    Console.WriteLine($"{kilograms} kg = {Math.Round(toLb)} lbs");
+                    break;
+            }
+        }
+    }
+}
